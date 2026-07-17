b4cd9b5f425f23d5be47e45ff9f4cc8692e3b6f3
anvijain28 <anvijain285@gmail.com>
2026-07-17 14:22:39 +0530

    Added Codeforces 1901A solution

---
+++ CF1901A.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class CF1901A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            int maxDistance = a[0]; // Distance from 0 to first gas station
+
+            // Maximum distance between consecutive gas stations
+            for (int i = 1; i < n; i++) {
+                maxDistance = Math.max(maxDistance, a[i] - a[i - 1]);
+            }
+
+            // Distance from last gas station to x and back
+            maxDistance = Math.max(maxDistance, 2 * (x - a[n - 1]));
+
+            System.out.println(maxDistance);
+        }
+
+        sc.close();
+    }
+}
