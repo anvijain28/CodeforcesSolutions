f4aa166ea49b304a654318e5105cb0232943d8b8
anvijain28 <anvijain285@gmail.com>
2026-07-24 12:51:19 +0530

    Added solution of Codeforces 1880A

---
+++ CF1881A.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            String x = sc.next();
+            String s = sc.next();
+
+            int ans = -1;
+
+            for (int i = 0; i <= 6; i++) {
+
+                if (x.contains(s)) {
+                    ans = i;
+                    break;
+                }
+
+                x = x + x;
+            }
+
+            System.out.println(ans);
+        }
+
+        sc.close();
+    }
+}
