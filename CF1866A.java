d65a239d3c7607337f101e5090ba06db941e0295
anvijain28 <anvijain285@gmail.com>
2026-07-28 23:19:14 +0530

    Added solution of Codeforces 1866A

---
+++ CF1866A.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            int x = sc.nextInt();
+
+            ans = Math.min(ans, Math.abs(x));
+        }
+
+        System.out.println(ans);
+
+        sc.close();
+    }
+}
