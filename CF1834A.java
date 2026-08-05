fae6fc7081a9740173af2434edc808b398746bee
anvijain28 <anvijain285@gmail.com>
2026-08-05 14:02:38 +0530

    Added solution of Codeforces 1834A

---
+++ CF1834A.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-- >0){
+            int n=sc.nextInt();
+
+            int neg=0;
+            int pos=0;
+
+            for(int i=0;i<n;i++){
+                int x =sc.nextInt();
+
+                if(x==-1)
+                neg++;
+                else
+                pos++;
+            
+            }
+
+         int ans =0;
+         //make sum non negative
+         while (pos<neg){
+            neg--;
+            pos++;
+            ans++;
+
+         }
+        
+        //Make product=1
+        if(neg%2==1)
+         ans++;
+
+         System.out.println(ans);
+
+    }
+
+    sc.close();
+}
+}
